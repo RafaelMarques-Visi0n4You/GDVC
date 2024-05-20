a875186a8e5728885ba5a7811a2bf69f2441d6b6
Branco95 <branquinho95@outlook.com>
2024-05-20 16:18:19 +0100

    /changed sequelize config

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,10 +1,12 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize(process.env.DB_NAME , process.env.DB_USER, process.env.DB_PASS, {
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false, // Remove this line if you don't want to see the raw SQL queries
-    });
+});
 
-module.exports = { sequelize };
+export { sequelize }; // Exporte sequelize como uma exportação nomeada
