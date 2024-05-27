9d9f353108512be50f2a7df5d761847693de9bcd
Didi446 <diogo2bcosta@gmail.com>
2024-05-27 17:06:40 +0100

    a

--- src/routes/ContaUtilizadorRouter.js
+++ src/routes/ContaUtilizadorRouter.js
@@ -11,7 +11,7 @@ router.put('/update/:id', verificarToken, updateContaUtilizador);
 router.put('/updateToken/:id', updateContaUtilizador);
 router.delete('/delete/:id', verificarToken, deleteContaUtilizador);
 router.put('/setAcesso/:id', verificarToken, setAcesso);
-router.get('/getcontasclientesempresa', getcontasclientesempresa);
+router.get('/getcontacliente', getcontasclientesempresa);
 
 
 export default router;
