4e048aa2d0e3ab1b14a12993c732cfd10fe15d85
Didi446 <diogo2bcosta@gmail.com>
2024-05-28 11:01:44 +0100

    asg

--- src/routes/ContaUtilizadorRouter.js
+++ src/routes/ContaUtilizadorRouter.js
@@ -6,7 +6,7 @@ import verificarToken from "../middleware/authMiddleware.js";
 router.get('/get', getContaUtilizadores);
 router.get('/get/:criado_por_id', getContaUtilizador);
 router.post('/getByEmpresa', getContaUtilizadoresEmpresa);
-router.post('/create', verificarToken, createContaUtilizador);
+router.post('/create', createContaUtilizador);
 router.put('/update/:id', verificarToken, updateContaUtilizador);
 router.put('/updateToken/:id', updateContaUtilizador);
 router.delete('/delete/:id', verificarToken, deleteContaUtilizador);
