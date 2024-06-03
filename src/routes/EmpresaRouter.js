58570fc65fc7631ffc7006de486ad986ef34630b
Didi446 <diogo2bcosta@gmail.com>
2024-06-03 14:03:22 +0100

    teste empresa

--- src/routes/EmpresaRouter.js
+++ src/routes/EmpresaRouter.js
@@ -5,7 +5,7 @@ import verificarToken from '../middleware/authMiddleware.js';
 
 
 router.post('/get', getEmpresas);
-router.get('/get/:id',verificarToken, getEmpresaById);
+router.get('/get/:id', getEmpresaById);
 router.post('/create', verificarToken, createEmpresa);
 router.put('/update/:id', verificarToken, updateEmpresa);
 router.delete('/delete/:id', verificarToken, deleteEmpresa);
