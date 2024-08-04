f2dae33927dfa95d237e3f5ee483110a6a5b04ba
Didi446 <diogo2bcosta@gmail.com>
2024-08-04 20:33:04 +0100

    teste

--- src/routes/ServicoRouter.js
+++ src/routes/ServicoRouter.js
@@ -4,7 +4,7 @@ import verificarToken from '../middleware/authMiddleware.js';
 import { getServicos, getServicosEmpresa, getServico, createServico, updateServico, deleteServico, setAcesso } from '../controllers/servicoController.js';
 
 router.get('/get', verificarToken, getServicos);
-router.post('/getServicosEmpresa', getServicosEmpresa);
+router.post('/getServicosEmpresa',verificarToken, getServicosEmpresa);
 router.get('/get/:id', verificarToken, getServico);
 router.post('/create', verificarToken, createServico);
 router.put('/update/:id', verificarToken, updateServico);
